8310a8c4f202490d46e276f922d5f2e8bc51bff4
Eduardo Ribeiro <dudub9go@hotmail.com>
2022-03-16 10:46:36 -0300

    Corrigindo erro de sintaxe nos repositorios

--- school/src/main/java/com/api/school/repository/EscolaRepository.java
+++ school/src/main/java/com/api/school/repository/EscolaRepository.java
@@ -1,8 +1,9 @@
 package com.api.school.repository;
 
+import com.api.school.model.Escola;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EscolaRepository extends JpaRepository<EscolaRepository, Long> {
+public interface EscolaRepository extends JpaRepository<Escola, Long> {
 }

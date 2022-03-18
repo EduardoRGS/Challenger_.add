b800762a202325a12d082092ea35b804688f33a0
Eduardo Ribeiro <dudub9go@hotmail.com>
2022-03-17 22:38:10 -0300

    Adicionando cadastro front-end de aluno utilizando thymeleaf

--- school/src/main/java/com/api/school/controller/AlunoController.java
+++ school/src/main/java/com/api/school/controller/AlunoController.java
@@ -1,21 +1,46 @@
 package com.api.school.controller;
 
 import com.api.school.model.Aluno;
+import com.api.school.repository.AlunoRepository;
 import com.api.school.service.AlunoService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
-@RestController
+@Controller
 @AllArgsConstructor
-@RequestMapping("aluno")
 @Slf4j
 public class AlunoController {
 
     AlunoService alunoService;
+    AlunoRepository alunoRepository;
+
+    @PostMapping("/escola/alunos/salvar")
+    public String createStudent (@Valid @ModelAttribute("aluno") Aluno aluno, BindingResult result, Model model){
+        if(result.hasErrors()){
+            return "/escola/alunos/form";
+        }
+        alunoRepository.save(aluno);
+        return "redirect:/escola/alunos";
+    }
+    @GetMapping("escola/alunos/novo")
+    public String newStudent(Model model){
+        model.addAttribute("aluno", new Aluno());
+        return "/escola/alunos/form";
+    }
+
+    @GetMapping("/escola/alunos")
+    public String listAllStudents (Model model){
+        model.addAttribute("listaAlunos", alunoRepository.findAll());
+        return "/escola/alunos/index";
+    }
 
     @PostMapping("cadastrar")
     public Aluno createStudent (@RequestBody Aluno aluno){
